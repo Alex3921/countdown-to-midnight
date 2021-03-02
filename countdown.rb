409c7fb73348d8f2fa277080f2ba9844c51f075a
alexandru <boncut.alex10@gmail.com>
2021-03-01 23:54:20 -0600

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,26 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    return "HAPPY NEW YEAR!" if n == 0
+  end
 end
+
+
+def countdown_with_sleep(n)
+    while n > 0
+      puts "#{n} SECOND(S)!"
+      n -= 1
+      sleep(1)
+      return "HAPPY NEW YEAR!" if n == 0
+    end  
+  end    
+
+
+countdown(10)
+countdown_with_sleep(10)
+
+
+
